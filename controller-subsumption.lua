5948938f93b4b2552a82c052e8e7ec9699425be6
Sambuzi <sajmir.buzi@studio.unibo.it>
2026-05-21 23:28:48 +0200

    fixed : improving code readability and maintainability

--- controller-subsumption.lua
+++ controller-subsumption.lua
@@ -1,27 +1,38 @@
-MAX_VELOCITY = 15
+local MAX_VELOCITY = 15
 
-TURN = 5
-BACKWARD = -3
+local TURN = 5
+local BACKWARD = -3
+local LIGHT_THRESHOLD = 0.05
+local OBSTACLE_THRESHOLD = 0.2
+local BLACK_THRESHOLD = 0.1
 
 function init()
     robot.leds.set_all_colors("green")
 end
 
-function go_to_light()
+function search_light()
+    return MAX_VELOCITY * 0.6, MAX_VELOCITY * 0.4
+end
+
+function go_to_light(l, r)
     local left = 0
     local right = 0
 
-    for i = 1, #robot.light do
-        if robot.light[i].angle > 0 then
-            left = left + robot.light[i].value
-        else
-            right = right + robot.light[i].value
-        end
+    for i = 1, 12 do
+        left = left + robot.light[i].value
     end
-    
-    if left > right + 0.05 then
+
+    for i = 13, 24 do
+        right = right + robot.light[i].value
+    end
+
+    if left + right < LIGHT_THRESHOLD then
+        return l, r
+    end
+
+    if left > right + LIGHT_THRESHOLD then
         return TURN, MAX_VELOCITY
-    elseif right > left + 0.05 then
+    elseif right > left + LIGHT_THRESHOLD then
         return MAX_VELOCITY, TURN
     else
         return MAX_VELOCITY, MAX_VELOCITY
@@ -32,19 +43,21 @@ function avoid_obstacles(l, r)
     local left = 0
     local right = 0
 
-    for i = 1, 3 do
+    for i = 1, 4 do
         left = left + robot.proximity[i].value
     end
 
-    for i = 22, 24 do
+    for i = 21, 24 do
         right = right + robot.proximity[i].value
     end
 
-    if left + right > 0.2 then
+    if left + right > OBSTACLE_THRESHOLD then
         if left > right then
             return MAX_VELOCITY, BACKWARD
-        else
+        elseif right > left then
             return BACKWARD, MAX_VELOCITY
+        else
+            return MAX_VELOCITY, BACKWARD
         end
     end
 
@@ -52,17 +65,24 @@ function avoid_obstacles(l, r)
 end
 
 function halt_on_black(l, r)
+    local black_sensors = 0
+
     for i = 1, #robot.motor_ground do
-        if robot.motor_ground[i].value < 0.1 then
-            return 0, 0
+        if robot.motor_ground[i].value < BLACK_THRESHOLD then
+            black_sensors = black_sensors + 1
         end
+    end
+
+    if black_sensors >= 2 then
+        return 0, 0
     end
 
     return l, r
 end
 
 function step()
-    local l, r = go_to_light()
+    local l, r = search_light()
+    l, r = go_to_light(l, r)
     l, r = avoid_obstacles(l, r)
     l, r = halt_on_black(l, r)
     robot.wheels.set_velocity(l, r)
